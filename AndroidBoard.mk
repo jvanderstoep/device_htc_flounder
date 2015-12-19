5fc6c84aea48b63615569e42210bd5ed2d4007ab
Daniel Micay <danielmicay@gmail.com>
2015-12-19 03:28:11 -0500

    add bootloader image
    bootloader: 3.48.0.0135

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := $(call my-dir)
+
 KERNEL_DEFCONFIG := flounder_defconfig
 KERNEL_DIR := kernel/htc/flounder
 
@@ -24,3 +26,5 @@ include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
 .PHONY: $(PRODUCT_OUT)/kernel
 $(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
 	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+
+$(call add-radio-file,radio/bootloader.img)
