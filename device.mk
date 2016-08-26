e2c817394312a8161193071f19f8d8235f45867e
Daniel Micay <danielmicay@gmail.com>
2016-08-26 08:03:32 -0400

    tmp disable vendor verity (incomplete on 9 anyway)

--- device.mk
+++ device.mk
@@ -282,7 +282,7 @@ endif
 $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_SUPPORTS_BOOT_SIGNER := false
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/APP
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
+#PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
 
 # for warning
 PRODUCT_PACKAGES += \
