30e4d0c0c5d75b9c691ec0de27ef41059f7eed83
Daniel Micay <danielmicay@gmail.com>
2016-08-27 15:14:23 -0400

    switch to in-tree kernel builds

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,3 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 $(call add-radio-file,radio/bootloader.img)
+
+KERNEL_DEFCONFIG := flounder_defconfig
+KERNEL_DIR := kernel/htc/flounder
+
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
